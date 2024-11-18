07862c4781f191cab6b2d4f4637b7157fa4f4723
Dorian Trazos <adrianheras@profesorestrazos.net>
2024-11-18 14:58:20 +0100

    updated

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+const menuIconElement = document.getElementById('menu-icon');
+const menuElement = document.getElementById('menu');
+
+const toggleMenu = () => {
+  menuElement.classList.toggle('menu-show');
+  document.body.classList.toggle('showing-menu');
+
+  if (menuElement.classList.contains('menu-show')) {
+    menuIconElement.src = './assets/icons/close.svg';
+  } else {
+    menuIconElement.src = './assets/icons/hamburger.svg';
+  }
+};
+
+menuIconElement.addEventListener('click', toggleMenu);
